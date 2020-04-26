23cb95afe9c482d970b5459af6a710f96f7966c9
Chiefbark <chiefbark.dev@gmail.com>
2020-04-26 19:40:05 +0200

    Updated export method

--- TFG/src/styles/index.js
+++ TFG/src/styles/index.js
@@ -1,12 +1,10 @@
-const colors = {
-	'primary': '#E91E63',
-	'primaryDark': '#C2185B',
-	'primaryLight': '#F8BBD0',
-	'secondary': '#FFFFFF',
-	'accent': '#757575',
-	'text': '#000000',
-	'textMuted': '#757575',
-	'separator': '#BDBDBD'
+export const colors = {
+	primary: '#E91E63',
+	primaryDark: '#C2185B',
+	primaryLight: '#F8BBD0',
+	secondary: '#FFFFFF',
+	accent: '#757575',
+	text: '#000000',
+	textMuted: '#757575',
+	separator: '#BDBDBD'
 }
-
-export default {colors};
