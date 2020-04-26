7f8cb773340492d4a2e8aee91dacb71e25a9288d
Chiefbark <chiefbark.dev@gmail.com>
2020-04-26 19:40:34 +0200

    Statistics screen added

---
+++ TFG/src/screens/statistics/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default class Calendar extends React.Component {
+	constructor(props) {
+		super(props);
+		
+	}
+	
+	render() {
+		return <View></View>;
+	}
+	
+}
