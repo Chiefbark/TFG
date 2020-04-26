0b286e0038ffe36b70783f8299835984a0c03716
Chiefbark <chiefbark.dev@gmail.com>
2020-04-26 23:37:01 +0200

    i18n spanish language

---
+++ TFG/src/i18n/es/index.js
@@ -0,0 +1,21 @@
+export const calendar = {
+	title: 'Calendario',
+	calendarLocales: {
+		monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+		monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sept', 'Oct', 'Nov', 'Dec'],
+		dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+		dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab']
+	}
+}
+export const statistics = {
+	title: 'Estadísticas'
+}
+export const absences = {
+	title: 'Ausencias'
+}
+export const profile = {
+	title: 'Perfil'
+}
+export const settings = {
+	title: 'Ajustes'
+}
