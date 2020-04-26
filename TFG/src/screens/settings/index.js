19c98c8aafd26d1bc02e54c8aa74bc46765cdd15
Chiefbark <chiefbark.dev@gmail.com>
2020-04-26 23:43:18 +0200

    Added Stack extends & functionality to change language

--- TFG/src/screens/settings/index.js
+++ TFG/src/screens/settings/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import * as i18n from '../../i18n';
+import CommonStack from "../commons/stack";
+
 import Button from '../../components/button';
 
-export default class Calendar extends React.Component {
+export default class SettingsScreen extends CommonStack {
 	constructor(props) {
 		super(props);
-		
+		this.state = {
+			key: 'settings'
+		}
 	}
 	
 	render() {
-		return <View></View>;
+		return <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+			<Button label={'Español'} onClick={() => i18n.setLocale('es')}/>
+			<Button label={'Inglés'} onClick={() => i18n.setLocale('en')}/>
+		</View>;
 	}
 	
 }
