91caa25d7f7079dde789182aa88b8f635c6e81c4
Chiefbark <chiefbark.dev@gmail.com>
2020-04-26 23:42:20 +0200

    Implemented Stack extends

--- TFG/src/screens/profile/index.js
+++ TFG/src/screens/profile/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import * as i18n from '../../i18n';
+import CommonStack from "../commons/stack";
 
-export default class Calendar extends React.Component {
+export default class ProfileScreen extends CommonStack {
 	constructor(props) {
 		super(props);
-		
+		this.state = {
+			key: 'profile'
+		}
 	}
 	
 	render() {
