898177887c5f18e5a70104616339d2b007b17a6e
Chiefbark <chiefbark.dev@gmail.com>
2020-04-26 23:42:43 +0200

    Implemented Stack entends & caldendar view

--- TFG/src/screens/calendar/index.js
+++ TFG/src/screens/calendar/index.js
@@ -1,14 +1,97 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import {LocaleConfig, Calendar} from 'react-native-calendars';
+import * as i18n from '../../i18n';
+import {colors} from "../../styles";
+import Icon from "../../components/icon";
+import CommonStack from "../commons/stack";
+import CalendarDay from "../../components/calendarDay";
 
-export default class Calendar extends React.Component {
+const markedDates = {
+	"2020-04-15": {
+		exams: [{color: "black", textColor: "white"}, {color: "red", textColor: "white"},
+			{color: "blue", textColor: "white"}, {color: "green", textColor: "white"}],
+	},
+	"2020-04-22": {
+		holiday: {
+			color: "pink",
+			startingDay: true,
+			textColor: "white"
+		},
+		absences: [
+			{color: 'green'},
+			{color: 'blue'},
+			{color: 'yellow'},
+			{color: 'red'},
+			{color: 'purple'},
+		]
+	}, "2020-04-23": {
+		holiday: {
+			color: "pink",
+			textColor: "white"
+		},
+	}, "2020-04-24": {
+		holiday: {
+			color: "pink",
+			endingDay: true,
+			textColor: "white"
+		},
+	}
+};
+
+LocaleConfig.locales[i18n.locale] = i18n.get('calendar').calendarLocales;
+LocaleConfig.defaultLocale = i18n.locale;
+
+export default class CalendarScreen extends CommonStack {
 	constructor(props) {
 		super(props);
-		
+		this.state = {
+			key: 'calendar'
+		}
+	}
+	
+	_onLocaleChange(locale) {
+		super._onLocaleChange(locale);
+		if (!LocaleConfig.locales[locale])
+			LocaleConfig.locales[locale] = i18n.get('calendar').calendarLocales;
+		LocaleConfig.defaultLocale = i18n.locale;
+	}
+	
+	componentDidMount() {
+		super.componentDidMount();
+		this.props.navigation.setOptions({
+			headerRight: () =>
+				<Icon source={require('../../../assets/icons/icon_help.png')} iconColor={colors.secondary}
+					  style={{marginRight: 16}}/>
+		});
 	}
 	
 	render() {
-		return <View></View>;
+		return <View>
+			<Calendar key={this.state.locale}
+					  markedDates={markedDates}
+					  current={new Date()}
+					  minDate={'2020-01-01'}
+					  maxDate={'2021-01-01'}
+					  monthFormat={'yyyy MMMM'}
+					  firstDay={1}
+					  onPressArrowLeft={substractMonth => substractMonth()}
+					  onPressArrowRight={addMonth => addMonth()}
+					  theme={{
+						  arrowColor: colors.text,
+						  'stylesheet.calendar.main': {
+							  week: {
+								  marginTop: 1, marginBottom: 1,
+								  flexDirection: 'row', justifyContent: 'space-around'
+							  }
+						  }
+					  }}
+					  dayComponent={({date, state, marking}) => {
+						  return (
+							  <CalendarDay date={date} state={state} marking={marking}/>
+						  );
+					  }}
+			/>
+		</View>;
 	}
-	
 }
