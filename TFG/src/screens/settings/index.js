fedf8f18ea78f0bf38564800f5b4127600128b29
Chiefbark <chiefbark.dev@gmail.com>
2020-04-26 19:40:58 +0200

    Settings screen added

---
+++ TFG/src/screens/settings/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Button from '../../components/button';
+
+export default class Calendar extends React.Component {
+	constructor(props) {
+		super(props);
+		
+	}
+	
+	render() {
+		return <View></View>;
+	}
+	
+}
