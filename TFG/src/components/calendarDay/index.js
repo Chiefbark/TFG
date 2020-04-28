2eb2dd4e4fa7f0980832dccd4c675be36a87ec23
Chiefbark <chiefbark.dev@gmail.com>
2020-04-28 17:09:26 +0200

    Formated file

--- TFG/src/components/calendarDay/index.js
+++ TFG/src/components/calendarDay/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import {colors} from "../../styles";
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {colors} from '../../styles';
 
 export default class CalendarDay extends React.Component {
 	
