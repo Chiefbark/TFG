c352d26562243415cd49e511ac2dc4a49cbb2ac8
Chiefbark <chiefbark.dev@gmail.com>
2020-04-30 17:59:30 +0200

    Added change language functionality
    Also implemented the switches options, but with no functionality yet

--- TFG/src/screens/settings/index.js
+++ TFG/src/screens/settings/index.js
@@ -8,6 +8,7 @@ import ListHeader from "../../components/listHeader";
 import ListItem from "../../components/listItem";
 import {colors} from "../../styles";
 import Dialog from "../../components/dialog";
+import Switch from '../../components/switch';
 
 export default class SettingsScreen extends CommonStack {
 	constructor(props) {
@@ -23,6 +24,7 @@ export default class SettingsScreen extends CommonStack {
 			<Fragment>
 				<ScrollView style={{flex: 1}}>
 					<ListHeader label={i18n.get('settings.headers.0')}/>
+					{/*	CONFIG ABOUT LANGUAGE	*/}
 					<ListHeader label={i18n.get('settings.headers.1')}/>
 					<ListItem title={i18n.get(`commons.languages.${i18n.getSelectedLang()}.name`)}
 							  titleStyles={{fontWeight: 'normal'}}
@@ -34,9 +36,67 @@ export default class SettingsScreen extends CommonStack {
 							  }
 							  feedback={false}
 							  style={{paddingVertical: 4}}/>
+					{/*	CONFIG ABOUT NOTIFICATIONS	*/}
 					<ListHeader label={i18n.get('settings.headers.2')}/>
+					<ListItem title={i18n.get(`settings.items.0`)}
+							  titleStyles={{fontWeight: 'normal'}}
+							  rightItem={() =>
+								  <Switch
+									  initialValue={false}
+									  onChange={() => console.log(i18n.get(`settings.items.0`), 'changed value')}
+									  style={{marginRight: 8}}
+								  />
+							  }
+							  feedback={false}
+							  style={{paddingVertical: 8}}/>
+					<ListItem title={i18n.get(`settings.items.1`)}
+							  titleStyles={{fontWeight: 'normal'}}
+							  rightItem={() =>
+								  <Switch
+									  initialValue={true}
+									  onChange={() => console.log(i18n.get(`settings.items.1`), 'changed value')}
+									  style={{marginRight: 8}}
+								  />
+							  }
+							  feedback={false}
+							  style={{paddingVertical: 8}}/>
+					{/*	CONFIG ABOUT CALENDAR	*/}
 					<ListHeader label={i18n.get('settings.headers.3')}/>
+					<ListItem title={i18n.get(`settings.items.2`)}
+							  titleStyles={{fontWeight: 'normal'}}
+							  rightItem={() =>
+								  <Switch
+									  initialValue={true}
+									  onChange={() => console.log(i18n.get(`settings.items.2`), 'changed value')}
+									  style={{marginRight: 8}}
+								  />
+							  }
+							  feedback={false}
+							  style={{paddingVertical: 8}}/>
+					<ListItem title={i18n.get(`settings.items.3`)}
+							  titleStyles={{fontWeight: 'normal'}}
+							  rightItem={() =>
+								  <Switch
+									  initialValue={true}
+									  onChange={() => console.log(i18n.get(`settings.items.3`), 'changed value')}
+									  style={{marginRight: 8}}
+								  />
+							  }
+							  feedback={false}
+							  style={{paddingVertical: 8}}/>
+					<ListItem title={i18n.get(`settings.items.4`)}
+							  titleStyles={{fontWeight: 'normal'}}
+							  rightItem={() =>
+								  <Switch
+									  initialValue={true}
+									  onChange={() => console.log(i18n.get(`settings.items.4`), 'changed value')}
+									  style={{marginRight: 8}}
+								  />
+							  }
+							  feedback={false}
+							  style={{paddingVertical: 8}}/>
 				</ScrollView>
+				{/*	LANGUAGE DIALOG	*/}
 				<Dialog title={i18n.get('commons.languageDialog.title')}
 						content={() =>
 							<Fragment>
