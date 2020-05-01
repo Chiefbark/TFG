6a88529f2160cd388437d5f0327b98a92248b368
Chiefbark <chiefbark.dev@gmail.com>
2020-05-01 21:51:03 +0200

    Implemented active value to only update one of them when chaging lang

--- TFG/src/screens/profile/information.js
+++ TFG/src/screens/profile/information.js
@@ -6,19 +6,28 @@ export default class InformationScreen extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			_active: false,
 			_locale: i18n.currLocale,
 			_lastModified: undefined
 		}
 	}
 	
 	_updateComponent() {
-		this.setState({_locale: i18n.currLocale, _lastModified: new Date().getTime()});
-		// this.props.navigation.dangerouslyGetParent().setOptions({title: i18n.get('absences.title')});
+		if (this.state._active)
+			this.setState({_locale: i18n.currLocale, _lastModified: new Date().getTime()});
+	}
+	
+	_onFocusComponent() {
+		this.setState({_active: true});
+		this.props.navigation.dangerouslyGetParent().setOptions({title: i18n.get('profile.screens.0.title')});
+		this.props.navigation.dangerouslyGetParent().dangerouslyGetParent().setOptions({tabBarLabel: i18n.get('profile.title')});
 	}
 	
 	componentDidMount() {
 		i18n.addListener(this._updateComponent.bind(this));
-		this._updateComponent();
+		this.props.navigation.addListener('focus', () => this._onFocusComponent());
+		this.props.navigation.addListener('blur', () => this.setState({_active: false}));
+		this._onFocusComponent();
 	}
 	
 	componentWillUnmount() {
