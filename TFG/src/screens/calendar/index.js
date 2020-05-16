6529a7ce62b4d7ea0185fa0c9adaecea1b3810f4
Chiefbark <chiefbark.dev@gmail.com>
2020-05-16 20:16:10 +0200

    Implemented show subjects or holidays when pressing a day & added marking

--- TFG/src/screens/calendar/index.js
+++ TFG/src/screens/calendar/index.js
@@ -1,11 +1,11 @@
 import React, {Fragment} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
 import * as i18n from '../../i18n';
 import * as config from '../../config';
 import * as firebase from '../../firebase';
-import {compareTimes, getDayOfWeek, getISODate} from '../../utils';
-import {colors} from '../../styles';
+import {compareTimes, getDayOfWeek, getDatesBetween, getISODate, isDateBetween} from '../../utils';
+import {colors, subjectColors} from '../../styles';
 
 import Toast from 'react-native-simple-toast';
 
@@ -83,6 +83,10 @@ export default class CalendarScreen extends React.Component {
 			let data = snapshot.val() || {};
 			this.setState({absences: Object.entries(data)}, () => this._updateMarking());
 		});
+		firebase.ref('holidays').on('value', snapshot => {
+			let data = snapshot.val() || {};
+			this.setState({holidays: Object.entries(data)}, () => this._updateMarking());
+		});
 		firebase.ref('subjects').on('value', () => this._updateMarking());
 	}
 	
@@ -95,13 +99,36 @@ export default class CalendarScreen extends React.Component {
 				marking[abs[ii][0]].multi = [];
 				const entries = Object.entries(abs[ii][1]);
 				for (let jj = 0; jj < entries.length; jj++) {
-					const id_subject = await firebase.ref('schedules').child(entries[jj][1]).child(entries[jj][0]).once('value').then(result => result.val().id_subject);
-					const color = await firebase.ref('subjects').child(id_subject).once('value').then(result => result.val().color);
-					marking[abs[ii][0]].multi.push({color: color});
+					const id_subject = await firebase.ref('schedules').child(entries[jj][1].path).child(entries[jj][0]).once('value').then(result => result.val()?.id_subject ?? undefined);
+					if (id_subject) {
+						const color = await firebase.ref('subjects').child(id_subject).once('value').then(result => result.val().color);
+						marking[abs[ii][0]].multi.push({color: color});
+					}
+				}
+			}
+		const hol = this.state.holidays;
+		if (hol)
+			for (let ii = 0; ii < hol.length; ii++) {
+				const dates = getDatesBetween(hol[ii][1].startDate, hol[ii][1].endDate);
+				for (let jj = 0; jj < dates.length; jj++) {
+					if (!marking[dates[jj]]) marking[dates[jj]] = {};
+					if (!marking[dates[jj]].selection) marking[dates[jj]].selection = {};
+					let obj = {...marking[dates[jj]].selection};
+					obj.color = colors.primaryLight;
+					if (!marking[dates[jj]].selection.color) {
+						if (dates[jj] == hol[ii][1].startDate)
+							obj.isStart = true;
+						if (dates[jj] == hol[ii][1].endDate)
+							obj.isEnd = true;
+					} else if (marking[dates[jj]].selection.isStart && dates[jj] != hol[ii][1].startDate)
+						delete obj.isStart;
+					else if (marking[dates[jj]].selection.isEnd && dates[jj] != hol[ii][1].endDate)
+						delete obj.isEnd;
+					
+					marking[dates[jj]].selection = {...obj}
 				}
 			}
 		// TODO: EXAMS
-		// TODO: HOLIDAYS
 		this.setState({marking: marking})
 	}
 	
@@ -142,19 +169,29 @@ export default class CalendarScreen extends React.Component {
 											   let currSchedule = this.state.schedules.find(e => value.dateString >= e[1].startDate && value.dateString <= e[1].endDate);
 											   obj.dateString = value.dateString;
 						
-											   const dayOfWeek = getDayOfWeek(value.dateString);
-											   if (currSchedule[1][dayOfWeek]) {
-												   const sorted = Object.entries(currSchedule[1][dayOfWeek])
-													   .sort((arg0, arg1) => compareTimes(arg1[1].startTime, arg0[1].startTime));
-												   obj.subjects = [];
-												   for (let ii = 0; ii < sorted.length; ii++) {
-													   obj.subjects[ii] = {...sorted[ii][1]};
-													   obj.subjects[ii].id_schedule = sorted[ii][0];
-													   obj.subjects[ii].path = `${currSchedule[0]}/${dayOfWeek}`;
-													   obj.subjects[ii].name = await firebase.ref('subjects')
-														   .child(sorted[ii][1].id_subject)
-														   .once('value')
-														   .then(result => result.val()?.name ?? {});
+											   let holidays = this.state.holidays?.filter(e => isDateBetween(value.dateString, e[1].startDate, e[1].endDate))
+											   if (holidays.length > 0) {
+												   obj.holidays = [];
+												   holidays.forEach(e => {
+													   obj.holidays.push({id_holiday: e[0], name: e[1].name, endDate: e[1].endDate})
+												   })
+											   } else {
+												   const dayOfWeek = getDayOfWeek(value.dateString);
+												   if (currSchedule[1][dayOfWeek]) {
+													   const sorted = Object.entries(currSchedule[1][dayOfWeek])
+														   .sort((arg0, arg1) => compareTimes(arg1[1].startTime, arg0[1].startTime));
+													   obj.subjects = [];
+													   for (let ii = 0; ii < sorted.length; ii++)
+														   if (sorted[ii][1].id_subject) {
+															   obj.subjects[ii] = {...sorted[ii][1]};
+															   obj.subjects[ii].id_schedule = sorted[ii][0];
+															   obj.subjects[ii].path = `${currSchedule[0]}/${dayOfWeek}`;
+															   obj.subjects[ii].id_subject = sorted[ii][1].id_subject;
+															   obj.subjects[ii].name = await firebase.ref('subjects')
+																   .child(sorted[ii][1].id_subject)
+																   .once('value')
+																   .then(result => result.val()?.name ?? undefined);
+														   }
 												   }
 											   }
 											   this.setState({selected: obj});
@@ -164,6 +201,44 @@ export default class CalendarScreen extends React.Component {
 			/>
 			<View style={styles.helpTextContainer}><Text style={styles.helpText}>{i18n.get('calendar.helpText')}</Text></View>
 			<Dialog title={i18n.get('commons.helpDialog.title')}
+					content={() =>
+						<Fragment>
+							<View style={{flexDirection: 'row', alignItems: 'center', marginVertical: 5}}>
+								<View style={{
+									width: 20, height: 20, borderRadius: 1000,
+									backgroundColor: subjectColors[6], marginHorizontal: 5, marginRight: 10
+								}}/>
+								<Text style={{flex: 1, flexWrap: 'wrap'}}>{i18n.get('calendar.helpDialog.placeholders.0')}</Text>
+							</View>
+							<View style={{flexDirection: 'row', alignItems: 'center', marginVertical: 5}}>
+								<View style={{
+									width: 25, height: 25, borderRadius: 1000,
+									backgroundColor: colors.primaryLight, marginHorizontal: 2.5, marginRight: 10
+								}}/>
+								<Text style={{flex: 1, flexWrap: 'wrap'}}>{i18n.get('calendar.helpDialog.placeholders.1')}</Text>
+							</View>
+							<View style={{flexDirection: 'row', alignItems: 'center', marginVertical: 5}}>
+								<View style={{width: 30, flexDirection: 'row', flexWrap: 'wrap', marginRight: 10}}>
+									<View style={{
+										width: 10, height: 10, borderRadius: 1000, backgroundColor: subjectColors[1], margin: 2.5
+									}}/>
+									<View style={{
+										width: 10, height: 10, borderRadius: 1000, backgroundColor: subjectColors[6], margin: 2.5
+									}}/>
+									<View style={{
+										width: 10, height: 10, borderRadius: 1000, backgroundColor: subjectColors[12], margin: 2.5
+									}}/>
+									<View style={{
+										width: 10, height: 10, borderRadius: 1000, backgroundColor: subjectColors[8], margin: 2.5
+									}}/>
+								</View>
+								<Text style={{flex: 1, flexWrap: 'wrap'}}>{i18n.get('calendar.helpDialog.placeholders.2')}</Text>
+							</View>
+							<Text style={{textAlign: 'center', color: colors.grey, marginTop: 10}}>
+								{i18n.get('calendar.helpDialog.placeholders.3')}
+							</Text>
+						</Fragment>
+					}
 					buttons={() =>
 						<Button label={i18n.get('commons.helpDialog.actions.0')}
 								backgroundColor={colors.primary} textColor={colors.white}
@@ -174,40 +249,55 @@ export default class CalendarScreen extends React.Component {
 			<Dialog title={getISODate(this.state.selected.dateString)}
 					content={() =>
 						<Fragment>
-							{this.state.selected.subjects &&
+							{!this.state.selected.holidays && this.state.selected.subjects &&
 							<Text style={{textAlign: 'center', color: colors.grey}}>
 								{i18n.get('calendar.absencesDialog.placeholders.0')}
 							</Text>
 							}
-							{this.state.selected.subjects?.map(e => {
-									let selected = undefined;
-									if (this.state.absences) {
-										const arr = this.state.absences.find(x => x[0] === this.state.selected.dateString);
-										if (arr) selected = Object.keys(arr[1]).find(x => x === e.id_schedule)
-									}
-									return (
-										<ListItem key={e.id_schedule} title={e.name} subtitle={`${e.startTime} - ${e.endTime}`}
-												  containerStyle={{paddingHorizontal: 0}}
-												  rightItem={() =>
-													  <Icon source={require('../../../assets/icons/icon_check.png')}
-															size={'small'} disabled={true} iconColor={selected ? colors.primary : colors.white}
-															style={{
-																borderWidth: 1, borderColor: colors.primary, borderRadius: 1000,
-																padding: 10, marginLeft: 8
-															}}/>
-												  }
-												  onClick={() => {
-													  if (selected)
-														  firebase.ref('absences').child(this.state.selected.dateString).child(e.id_schedule).remove()
-															  .then(() => Toast.showWithGravity(i18n.get('calendar.absenceChanged.1'), Toast.SHORT, Toast.BOTTOM));
-													  else
-														  firebase.ref('absences').child(this.state.selected.dateString).child(e.id_schedule).set(e.path)
-															  .then(() => Toast.showWithGravity(i18n.get('calendar.absenceChanged.0'), Toast.SHORT, Toast.BOTTOM));
-												  }}
-										/>
-									)
+							{!this.state.selected.subjects && this.state.selected.holidays &&
+							<View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+								<Text style={{textAlign: 'left', color: colors.grey}}>
+									{i18n.get('calendar.holidaysDialog.placeholders.0')}
+								</Text>
+								<Image source={require('../../../assets/icons/icon_party.png')} style={{width: 24, height: 24}}/>
+							</View>
+							}
+							{this.state.selected.holidays?.map(e =>
+								<ListItem key={e.id_holiday} title={e.name} feedback={false}
+										  subtitle={`${i18n.get('calendar.holidaysDialog.placeholders.1')} ${getISODate(e.endDate)}`}
+										  containerStyle={{paddingHorizontal: 0}}/>
+							)}
+							{!this.state.selected.holidays && this.state.selected.subjects?.map(e => {
+								let selected = undefined;
+								if (this.state.absences) {
+									const arr = this.state.absences.find(x => x[0] === this.state.selected.dateString);
+									if (arr) selected = Object.keys(arr[1]).find(x => x === e.id_schedule)
 								}
-							) ?? <Text>{i18n.get('calendar.emptySchedule')}</Text>}
+								return e.name &&
+									<ListItem key={e.id_schedule} title={e.name}
+											  subtitle={`${e.startTime} - ${e.endTime}`}
+											  containerStyle={{paddingHorizontal: 0}}
+											  rightItem={() =>
+												  <Icon source={require('../../../assets/icons/icon_check.png')}
+														size={'small'} disabled={true} iconColor={selected ? colors.primary : colors.white}
+														style={{
+															borderWidth: 1, borderColor: colors.primary, borderRadius: 1000,
+															padding: 10, marginLeft: 8
+														}}/>
+											  }
+											  onClick={() => {
+												  if (selected)
+													  firebase.ref('absences').child(this.state.selected.dateString).child(e.id_schedule).remove()
+														  .then(() => Toast.showWithGravity(i18n.get('calendar.absenceChanged.1'), Toast.SHORT, Toast.BOTTOM));
+												  else
+													  firebase.ref('absences').child(this.state.selected.dateString).child(e.id_schedule)
+														  .set({path: e.path, id_subject: e.id_subject})
+														  .then(() => Toast.showWithGravity(i18n.get('calendar.absenceChanged.0'), Toast.SHORT, Toast.BOTTOM));
+											  }}
+									/>
+							})}
+							{!this.state.selected.holidays && !this.state.selected.subjects &&
+							<Text>{i18n.get('calendar.emptySchedule')}</Text>}
 						</Fragment>
 					}
 					buttons={() =>
