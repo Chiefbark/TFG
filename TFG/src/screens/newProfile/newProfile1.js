6fa05f14fe26ad117d29da40eb1a9fe4c948d953
Chiefbark <chiefbark.dev@gmail.com>
2020-05-17 19:26:09 +0200

    Added exit button

--- TFG/src/screens/newProfile/newProfile1.js
+++ TFG/src/screens/newProfile/newProfile1.js
@@ -1,16 +1,14 @@
 import React, {Fragment} from 'react';
 
-import {View, Text, Image, BackHandler} from 'react-native';
+import {View, Text, Image, ActivityIndicator} from 'react-native';
 
 import * as i18n from '../../i18n';
 import * as config from '../../config';
-import * as firebase from '../../firebase';
 import {colors} from '../../styles';
 
 import {StackActions} from '@react-navigation/native';
 
 import Button from '../../components/button';
-import Dialog from '../../components/dialog';
 
 export default class NewProfile1 extends React.Component {
 	
@@ -20,17 +18,18 @@ export default class NewProfile1 extends React.Component {
 	}
 	
 	componentDidMount() {
-		BackHandler.addEventListener('hardwareBackPress', () => {
-			this.setState({dialogExit: true});
-			return true;
-		});
 		this.props.navigation.setOptions({title: i18n.get('newProfile.title')});
 	}
 	
 	render() {
 		return (
 			<Fragment>
-				<View style={{flex: 1, padding: 32, alignItems: 'center', justifyContent: 'flex-start'}}>
+				<View style={{position: 'relative', flex: 1, padding: 32, alignItems: 'center', justifyContent: 'flex-start'}}>
+					<ActivityIndicator size={25} animating={this.state.loading} color={colors.primary}
+									   style={{
+										   backgroundColor: colors.white, borderRadius: 1000,
+										   opacity: this.state.loading ? 1 : 0, padding: 5
+									   }}/>
 					<View style={{flexDirection: 'row', paddingHorizontal: 32, alignItems: 'center', paddingVertical: 12}}>
 						<Image source={require('../../../assets/icons/icon_wizard_art.png')} style={{width: 32, height: 32}}/>
 						<Text style={{fontSize: 18, fontWeight: 'bold', textAlign: 'center', paddingHorizontal: 16}}>
@@ -41,32 +40,22 @@ export default class NewProfile1 extends React.Component {
 					<Text style={{textAlign: 'center', marginTop: 16, marginBottom: 100}}>
 						{i18n.get('newProfile.screens.0.description.0')}
 					</Text>
+					<Button label={i18n.get('newProfile.screens.0.prevButton')}
+							textColor={colors.primary}
+							onClick={() => {
+								this.setState({loading: true})
+								setTimeout(async () =>
+										config.setNavigation('default')
+									, 0)
+							}}
+							style={{position: 'absolute', bottom: 50, left: 16}}
+					/>
 					<Button label={i18n.get('newProfile.screens.0.nextButton') + '  ➤'} backgroundColor={colors.primary}
 							textColor={colors.white}
 							onClick={() => this.props.navigation.dispatch(StackActions.push('NewProfile2'))}
 							style={{position: 'absolute', bottom: 50, right: 16, borderRadius: 1000}}
 					/>
 				</View>
-				<Dialog title={i18n.get('newProfile.screens.0.exitDialog.title')} loading={this.state.loadingExit}
-						content={() => <Text>{i18n.get('newProfile.screens.0.exitDialog.description')}</Text>}
-						buttons={() =>
-							<Fragment>
-								<Button label={i18n.get('newProfile.screens.0.exitDialog.actions.0')}
-										onClick={() => this.setState({dialogExit: false})}/>
-								<Button label={i18n.get('newProfile.screens.0.exitDialog.actions.1')}
-										backgroundColor={colors.primary} textColor={colors.white}
-										onClick={() => {
-											this.setState({loadingExit: true})
-											setTimeout(async () => {
-												firebase.ref('currProfile').remove()
-												this.setState({dialogExit: false, loadingExit: false},
-													() => config.setNavigation('default'))
-											})
-										}}
-								/>
-							</Fragment>
-						}
-						visible={this.state.dialogExit}/>
 			</Fragment>
 		)
 	}
